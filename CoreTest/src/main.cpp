942b0428f4512c21aa82d90a4cd03ba26722ef91
TempleLo <templelo5408@gmail.com>
2021-09-26 16:14:04 +0800

    Set externals vers and sort Debug/Release mode.

--- CoreTest/src/main.cpp
+++ CoreTest/src/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "headers/framebuffer_postprocess.h"
 #include "headers/read_objmodel.h"
 
@@ -7,4 +9,5 @@ int main() {
 	read_objmodel::callable_main();
 	//framebuffer_postprocess::callable_main();
 	AQ_GlobalCtrl::LightsCtrl::addDirectionalLight(glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(.5f, .5f, .5f), 1.f);
+	std::cout << "Core Test End" << std::endl;
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Libraries/APIs used:<br/>
-1.GLFW<br/>
-2.GLAD<br/>
-3.GLM<br/>
-4.Assimp<br/>
-5.stbi_image<br/>
+1.GLFW 3.3.4<br/>
+2.GLAD 0.1.34<br/>
+3.GLM 0.9.9.8<br/>
+4.Assimp 5.0.1<br/>
+5.stbi_image 2.27<br/>
