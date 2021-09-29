5f09639c796dd0e1290c407e580d3f51c2d08cad
TempleLo <templelo5408@gmail.com>
2021-09-29 20:28:53 +0800

    Add Dear Imgui and run it in guitar model test program.

--- CoreTest/src/read_objmodel.cpp
+++ CoreTest/src/read_objmodel.cpp
@@ -1,20 +1,33 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
+#include "dear_imgui/imgui.h"
+#include "dear_imgui/imgui_impl_glfw.h"
+#include "dear_imgui/imgui_impl_opengl3.h"
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
 
 #include <headers/AQ_Shader.h>
 #include <headers/AQ_CompCamera.h>
 #include <headers/AQ_CompModel.h>
 #include <headers/AQ_GameObjectCtrl.h>
-
 #include <headers/stbi_image_wrapper.h>
 #include <headers/AQ_Database.h>
 #include <headers/AQ_GameObject.h>
 
 #include <iostream>
+
+// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
+// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
+// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
 
 //#define REMOVE_GUITAR_IN_SECONDS
 #ifdef REMOVE_GUITAR_IN_SECONDS
@@ -31,8 +44,8 @@ namespace read_objmodel {
     void countTime();
 
     // settings
-    const unsigned int SCR_WIDTH = 800;
-    const unsigned int SCR_HEIGHT = 600;
+    int SCR_WIDTH = 800;
+    int SCR_HEIGHT = 600;
 
     // camera
     AQ_GameObject cameraObject;
@@ -53,7 +66,7 @@ namespace read_objmodel {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
+        const char* glsl_version = "#version 330";
 
 #ifdef __APPLE__
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -81,6 +94,26 @@ namespace read_objmodel {
             std::cout << "Failed to initialize GLAD" << std::endl;
             return -1;
         }
+        // Setup Dear ImGui context
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO(); (void)io;
+        //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+        //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+        // Setup Dear ImGui style
+        ImGui::StyleColorsDark();
+        //ImGui::StyleColorsClassic();
+
+        // Setup Platform/Renderer backends
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init(glsl_version);
+
+        // ImGui State
+        bool show_demo_window = true;
+        bool show_another_window = false;
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
 
         // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
         setFlipVerticallyOnLoad(true);
@@ -115,7 +148,56 @@ namespace read_objmodel {
         // -----------
         while (!glfwWindowShouldClose(window)) {
             countTime();
-            
+
+            // Poll and handle events (inputs, window resize, etc.)
+            // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+            // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+            // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+            // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+            glfwPollEvents();
+
+            // Start the Dear ImGui frame
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+            if (show_demo_window)
+                ImGui::ShowDemoWindow(&show_demo_window);
+
+            // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+            {
+                static float f = 0.0f;
+                static int counter = 0;
+
+                ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+                ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+                ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+                ImGui::Checkbox("Another Window", &show_another_window);
+
+                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+                ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+                if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                    counter++;
+                ImGui::SameLine();
+                ImGui::Text("counter = %d", counter);
+
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+                ImGui::End();
+            }
+
+            // 3. Show another simple window.
+            if (show_another_window) {
+                ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+                ImGui::Text("Hello from another window!");
+                if (ImGui::Button("Close Me"))
+                    show_another_window = false;
+                ImGui::End();
+            }
+
+
             // input
                 // -----
             processInput(window);
@@ -153,15 +235,23 @@ namespace read_objmodel {
                 AQ_GameObjectCtrl::removeComponent<AQ_CompModel>(guitarObject, "GUITAR");
 #endif
 #endif
+            // Rendering
+            ImGui::Render();
+            glfwGetFramebufferSize(window, &SCR_WIDTH, &SCR_HEIGHT);
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-            // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+            // glfw: swap buffers
             // -------------------------------------------------------------------------------
             glfwSwapBuffers(window);
-            glfwPollEvents();
         }
 
+        // Cleanup
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
         // glfw: terminate, clearing all previously allocated GLFW resources.
         // ------------------------------------------------------------------
+        glfwDestroyWindow(window);
         glfwTerminate();
         return 0;
     }
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Libraries/APIs used:<br/>
 3.GLM 0.9.9.8<br/>
 4.Assimp 5.0.1<br/>
 5.stbi_image 2.27<br/>
+6.Dear ImGui 1.84.2<br/>
