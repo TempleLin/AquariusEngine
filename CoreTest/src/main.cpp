10175aff6b42c45c33f3aff47cba534bd4bc90d3
TempleLo <templelo5408@gmail.com>
2021-10-11 01:58:54 +0800

    Replace linking to vcpkg auto link; Switch compiler from Clang back to MSVC++.

--- CoreTest/src/main.cpp
+++ CoreTest/src/main.cpp
@@ -12,10 +12,10 @@
 #include <GLFW/glfw3.h>
 
 int main() {
-	//read_objmodel::callable_main();
+	read_objmodel::callable_main();
 	//framebuffer_postprocess::callable_main();
 	//AQ_GlobalCtrl::LightsCtrl::addDirectionalLight(glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(.5f, .5f, .5f), 1.f);
 	//gamma_correction::callable_main();
-	shadow_mapping::main();
+	//shadow_mapping::main();
 	std::cout << "Core Test End" << std::endl;
 }
