9f571465c595ad61c7be4e26fcd7260ef351b833
TempleLo <templelo5408@gmail.com>
2021-10-16 20:55:27 +0800

    Remove unecessary if std::is_base_of checking due to already existed checking in template.

--- AquariusCore/src/headers/AQ_GameObjectCtrl.hpp
+++ AquariusCore/src/headers/AQ_GameObjectCtrl.hpp
@@ -27,72 +27,60 @@ namespace aquarius_engine {
 
 		template <typename T, typename = std::enable_if_t<std::is_base_of<AQ_Component, T>::value>>
 		T* addComponent(AQ_GameObject* gameObject, T* component, std::string name) {
-			if (std::is_base_of<AQ_Component, T>::value) {
-				try {
-					component->name = name;
-					/*
-					* @Add the component with to the database and receive its key in database by returning in to
-					*  component object's databaseAccessKey.
-					*/
-					databaseComponent->addComponent(component, component->databaseAccessKey);
-					const auto& componentsKeysVecRef = gameObject->componentsKeys[typeid(T)];
-					// @Check if the name specified already exists in the gameobject's components.
-					for (int i = 0; i < componentsKeysVecRef.size(); i++) {
-						if (componentsKeysVecRef.at(i).first == name) {
-							throw std::string("ERROR: GAMEOBJECT'S SAME COMPONENT TYPE ALREADY HAS ONE WITH THE NAME");
-						}
+			try {
+				component->name = name;
+				/*
+				* @Add the component with to the database and receive its key in database by returning in to
+				*  component object's databaseAccessKey.
+				*/
+				databaseComponent->addComponent(component, component->databaseAccessKey);
+				const auto& componentsKeysVecRef = gameObject->componentsKeys[typeid(T)];
+				// @Check if the name specified already exists in the gameobject's components.
+				for (int i = 0; i < componentsKeysVecRef.size(); i++) {
+					if (componentsKeysVecRef.at(i).first == name) {
+						throw std::string("ERROR: GAMEOBJECT'S SAME COMPONENT TYPE ALREADY HAS ONE WITH THE NAME");
 					}
-					// @Save name and access key to database descriptions of the component to the gameobject.
-					gameObject->componentsKeys[typeid(T)].push_back(std::pair<std::string, unsigned int>(name, component->databaseAccessKey));
-					return component;
-				} catch (std::string& errorMessage) {
-					std::cout << errorMessage << "\n";
-					return nullptr;
-				} catch (std::out_of_range& e) {
-					std::cout << "ERROR: CANNOT ADD COMPONENT FROM GAMEOBJECT" << e.what() << "\n";
-					return nullptr;
-				} catch (...) {
-					std::cout << "ERROR: UNKNOW ERROR IN ADDCOMPONENT FROM AQ_GAMEOBJECTCTRL" << "\n";
-					return nullptr;
 				}
-			} else {
-				std::cout << "Specified type from addComponent is not a component type." << "\n";
+				// @Save name and access key to database descriptions of the component to the gameobject.
+				gameObject->componentsKeys[typeid(T)].push_back(std::pair<std::string, unsigned int>(name, component->databaseAccessKey));
+				return component;
+			} catch (std::string& errorMessage) {
+				std::cout << errorMessage << "\n";
+				return nullptr;
+			} catch (std::out_of_range& e) {
+				std::cout << "ERROR: CANNOT ADD COMPONENT FROM GAMEOBJECT" << e.what() << "\n";
+				return nullptr;
+			} catch (...) {
+				std::cout << "ERROR: UNKNOW ERROR IN ADDCOMPONENT FROM AQ_GAMEOBJECTCTRL" << "\n";
 				return nullptr;
 			}
 		}
 
 		template <typename T, typename = std::enable_if_t<std::is_base_of<AQ_Component, T>::value>>
 		T* getComponent(AQ_GameObject* gameObject, std::string name) {
-			if (std::is_base_of<AQ_Component, T>::value) {
-				try {
-					const auto& componentsKeysVecRef = gameObject->componentsKeys[typeid(T)];
-					return std::any_cast<T*>(databaseComponent->allComponents
-						.at(componentsKeysVecRef.at(getComponentIndex(componentsKeysVecRef, name)).second));
-				} catch (std::out_of_range& e) {
-					std::cout << "FAILED TO GET COMPONENT FROM GAMEOBJECT CTRL" << e.what() << "\n";
-					return nullptr;
-				} catch (...) {
-					std::cout << "ERROR: UNKNOW ERROR FROM GETTING COMPONENT IN AQ_GAMEOBJECTCTRL" << "\n";
-					return nullptr;
-				}
-			} else {
-				std::cout << "Specified type from getComponent is not a component type." << "\n";
+			try {
+				const auto& componentsKeysVecRef = gameObject->componentsKeys[typeid(T)];
+				return std::any_cast<T*>(databaseComponent->allComponents
+					.at(componentsKeysVecRef.at(getComponentIndex(componentsKeysVecRef, name)).second));
+			} catch (std::out_of_range& e) {
+				std::cout << "FAILED TO GET COMPONENT FROM GAMEOBJECT CTRL" << e.what() << "\n";
+				return nullptr;
+			} catch (...) {
+				std::cout << "ERROR: UNKNOW ERROR FROM GETTING COMPONENT IN AQ_GAMEOBJECTCTRL" << "\n";
 				return nullptr;
 			}
 		}
 
 		template <typename T, typename = std::enable_if_t<std::is_base_of<AQ_Component, T>::value>>
 		void removeComponent(AQ_GameObject* gameObject, std::string name) {
-			if (std::is_base_of<AQ_Component, T>::value) {
-				try {
-					const auto& componentsKeysVecRef = gameObject->componentsKeys[typeid(T)];
-					databaseComponent->allComponents.erase
-					(componentsKeysVecRef.at(getComponentIndex(componentsKeysVecRef, name)).second);
-				} catch (std::out_of_range& e) {
-					std::cout << "FAILED TO REMOVE COMPONENT FROM GAMEOBJECT CTRL" << e.what() << "\n";
-				} catch (...) {
-					std::cout << "ERROR: UNKNOW ERROR FROM REMOVING COMPONENT IN AQ_GAMEOBJECTCTRL" << "\n";
-				}
+			try {
+				const auto& componentsKeysVecRef = gameObject->componentsKeys[typeid(T)];
+				databaseComponent->allComponents.erase
+				(componentsKeysVecRef.at(getComponentIndex(componentsKeysVecRef, name)).second);
+			} catch (std::out_of_range& e) {
+				std::cout << "FAILED TO REMOVE COMPONENT FROM GAMEOBJECT CTRL" << e.what() << "\n";
+			} catch (...) {
+				std::cout << "ERROR: UNKNOW ERROR FROM REMOVING COMPONENT IN AQ_GAMEOBJECTCTRL" << "\n";
 			}
 		}
 
