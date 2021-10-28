6b14398c354b7df770e2676f0dbba93c2c95354a
TempleLo <templelo5408@gmail.com>
2021-10-29 03:40:12 +0800

    Rename AQ_Comp2DModel to AQ_CompSimple2D; Incomplete implementation of AQ_CompSimple2D.

---
+++ AquariusCore/src/headers/AQ_CompSimple2D.hpp
@@ -0,0 +1,80 @@
+#pragma once
+#include "AQ_Component.hpp"
+#include "stbi_image_wrapper.hpp"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <vector>
+#include <string>
+
+namespace aquarius_engine {
+	class AQ_CompSimple2D : public AQ_Component {
+	private:
+		struct TextureNamePair {
+		public:
+			std::string name;
+			int width, height, nrChannels;
+			unsigned int texture;
+			TextureNamePair(std::string name, int width, int height, int nrChannels, unsigned int texture)
+				: name(name), width(width), height(height), nrChannels(nrChannels), texture(texture) {
+			}
+		};
+		unsigned int vao, vbo, ebo, shaderID;
+		unsigned int* uniforms;
+		char** uniformsNames;
+		std::vector<TextureNamePair> textures;
+	public:
+
+		AQ_CompSimple2D(unsigned int vao, unsigned int vbo, unsigned int ebo) : vao(vao), vbo(vbo), ebo(ebo) {
+			
+		}
+		static void enableAlpha() {
+			/*
+			* @Note: These need to be set to have transparent background image.
+			*/
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+		}
+		void addTexture(std::string imageLocation, stbi_image_wrap::ImageLoadingType, std::string name, bool hasAndUseAlpha, bool bindTexture) {
+			for (int i = 0; i < textures.size(); i++) {
+				if (textures.at(i).name == name) {
+					throw std::string("ERROR: IMAGE ADDED WAS SPECIFIED A NAME THAT ALREADY EXISTS");
+				}
+			}
+			unsigned int texture;
+			glGenTextures(1, &texture);
+			glBindTexture(GL_TEXTURE_2D, texture);
+			int width, height, nrChannels;
+			unsigned char* data = stbi_image_wrap::loadImage(imageLocation, width, height, nrChannels);
+			if (data) {
+				if (!hasAndUseAlpha)
+					glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+				else
+					glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+				textures.push_back(TextureNamePair(name, width, height, nrChannels, texture));
+				stbi_image_wrap::freeImage(data);
+				if (!bindTexture)
+					glBindTexture(GL_TEXTURE_2D, 0);
+			} else {
+				throw std::string("ERROR: CANNOT LOAD IMAGE WITH GIVEN PATH--" + imageLocation);
+			}
+		}
+		void setShaderID(unsigned int shaderID) {
+			this->shaderID = shaderID;
+		}
+		void setUniforms(unsigned int* uniforms, char** uniformsNames) {
+			this->uniforms = uniforms;
+			this->uniformsNames = uniformsNames;
+		}
+		void bindTexture(std::string textureName) {
+			for (int i = 0; i < textures.size(); i++) {
+				if (textures.at(i).name == textureName) {
+					glBindTexture(GL_TEXTURE_2D, textures.at(i).texture);
+				}
+			}
+		}
+		~AQ_CompSimple2D() {
+			delete[] uniforms;
+			delete[] uniformsNames;
+		}
+	};
+}
