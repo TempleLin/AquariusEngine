5da124bdb5255f28f8b9b07c2ee067e9aef9afd0
TempleLo <templelo5408@gmail.com>
2021-11-11 03:00:25 +0800

    Incomplete and untested first DearImgui wrap from JAquariusEngine.

---
+++ JAquariusEngine/src/EngineCore/AQ_UniControls.java
@@ -0,0 +1,16 @@
+package EngineCore;
+
+public class AQ_UniControls extends KeyToCPPEngineCore{
+    private long gameObjectCtrl;
+    private long globalCtrl;
+
+    public AQ_UniControls(long scene){
+
+    }
+    public AQ_GameObjectCtrl getGameObjectCtrl(){
+        throw null;
+    }
+    public AQ_GlobalCtrl getGlobalCtrl(){
+        throw null;
+    }
+}
---
+++ JAquariusEngine/src/EngineCore/MainTest.java
@@ -0,0 +1,10 @@
+package EngineCore;
+
+public class MainTest {
+    static{
+        System.loadLibrary("AquariusJNIWrap");
+    }
+    public static void main(String[] args) {
+
+    }
+}
