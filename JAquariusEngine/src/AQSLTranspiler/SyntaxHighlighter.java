9169cceca608cd8950e24e49f1ad7f35914adb57
TempleLo <templelo5408@gmail.com>
2021-11-13 01:36:38 +0800

    Much better text color sensing color change feature.

--- JAquariusEngine/src/AQSLTranspiler/SyntaxHighlighter.java
+++ JAquariusEngine/src/AQSLTranspiler/SyntaxHighlighter.java
@@ -1,28 +1,77 @@
 package AQSLTranspiler;
 
 import javax.swing.*;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.Style;
-import javax.swing.text.StyleConstants;
-import javax.swing.text.StyledDocument;
+import javax.swing.text.*;
 import java.awt.*;
 
 public class SyntaxHighlighter {
     JTextPane textPane;
-    StyledDocument styledDocument; //For inserting text with or without colors.
+    DefaultStyledDocument defaultStyledDocument;
     Style style;
     public SyntaxHighlighter(JTextPane textPane){
         this.textPane = textPane;
-        styledDocument = textPane.getStyledDocument();
-        style = textPane.addStyle("First style test", null);
 
-        try{
-            StyleConstants.setForeground(style, Color.RED);
-            styledDocument.insertString(styledDocument.getLength(), "Test0\n", style);
-            StyleConstants.setForeground(style, Color.BLUE);
-            styledDocument.insertString(styledDocument.getLength(), "Test1\n", style);
-        } catch (BadLocationException e) {
-            e.printStackTrace();
+        final StyleContext cont = StyleContext.getDefaultStyleContext();
+        final AttributeSet attr = cont.addAttribute(cont.getEmptySet(), StyleConstants.Foreground, Color.CYAN);
+        final AttributeSet attrBlack = cont.addAttribute(cont.getEmptySet(), StyleConstants.Foreground, Color.WHITE);
+        DefaultStyledDocument doc = new DefaultStyledDocument() {
+            public void insertString (int offset, String str, AttributeSet a) throws BadLocationException {
+                super.insertString(offset, str, a);
+
+                String text = getText(0, getLength());
+                int before = findLastNonWordChar(text, offset);
+                if (before < 0) before = 0;
+                int after = findFirstNonWordChar(text, offset + str.length());
+                int wordL = before;
+                int wordR = before;
+
+                while (wordR <= after) {
+                    if (wordR == after || String.valueOf(text.charAt(wordR)).matches("\\W")) {
+                        if (text.substring(wordL, wordR).matches("(\\W)*(private|public|protected)"))
+                            setCharacterAttributes(wordL, wordR - wordL, attr, false);
+                        else
+                            setCharacterAttributes(wordL, wordR - wordL, attrBlack, false);
+                        wordL = wordR;
+                    }
+                    wordR++;
+                }
+            }
+
+            public void remove (int offs, int len) throws BadLocationException {
+                super.remove(offs, len);
+
+                String text = getText(0, getLength());
+                int before = findLastNonWordChar(text, offs);
+                if (before < 0) before = 0;
+                int after = findFirstNonWordChar(text, offs);
+
+                if (text.substring(before, after).matches("(\\W)*(private|public|protected)")) {
+                    setCharacterAttributes(before, after - before, attr, false);
+                } else {
+                    setCharacterAttributes(before, after - before, attrBlack, false);
+                }
+            }
+        };
+        textPane.setDocument(doc);
+        defaultStyledDocument = (DefaultStyledDocument) textPane.getStyledDocument();
+    }
+
+    private int findLastNonWordChar (String text, int index) {
+        while (--index >= 0) {
+            if (String.valueOf(text.charAt(index)).matches("\\W")) {
+                break;
+            }
         }
+        return index;
+    }
+
+    private int findFirstNonWordChar (String text, int index) {
+        while (index < text.length()) {
+            if (String.valueOf(text.charAt(index)).matches("\\W")) {
+                break;
+            }
+            index++;
+        }
+        return index;
     }
 }
