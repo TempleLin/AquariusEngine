b9ffa5750a9590f0f078b3a52e368beb54c873ef
TempleLo <templelo5408@gmail.com>
2021-11-20 17:32:35 +0800

    Remove unecessary window.

--- AquariusEngine/TwoDJavaGameIntegrate/TwoDJavaGameIntegrate.cpp
+++ AquariusEngine/TwoDJavaGameIntegrate/TwoDJavaGameIntegrate.cpp
@@ -178,27 +178,27 @@ int main()
 
             }
 
-            static float f = 0.0f;
-            static int counter = 0;
+            //static float f = 0.0f;
+            //static int counter = 0;
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-            ImVec2 windowSize(336.f, 210.f);
-            ImGui::SetWindowSize(windowSize);
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            //ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            //ImGui::Checkbox("Another Window", &show_another_window);
+            //ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+            //ImVec2 windowSize(336.f, 210.f);
+            //ImGui::SetWindowSize(windowSize);
+            //ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ////ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ////ImGui::Checkbox("Another Window", &show_another_window);
 
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            //ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+            //ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ////ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
+            //if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            //    counter++;
+            //ImGui::SameLine();
+            //ImGui::Text("counter = %d", counter);
 
-            ImGui::Text("Seconds passed in game: %u", (unsigned int)timeCtrl->getSecondsInGame());
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
+            //ImGui::Text("Seconds passed in game: %u", (unsigned int)timeCtrl->getSecondsInGame());
+            //ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            //ImGui::End();
         }
 
         timeCtrl->updateTime();
