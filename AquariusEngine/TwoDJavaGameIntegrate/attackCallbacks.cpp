af757a842e77f501be420080317d8177b380f749
TempleLo <templelo5408@gmail.com>
2021-11-24 19:52:23 +0800

    Move selection page components initialization code to function.

--- AquariusEngine/TwoDJavaGameIntegrate/attackCallbacks.cpp
+++ AquariusEngine/TwoDJavaGameIntegrate/attackCallbacks.cpp
@@ -8,6 +8,8 @@ namespace attack {
 	void monster0BtnPreDrawCallback(unsigned int shaderID, AQ_CompSimpleBox2D* simpleBox2DThis);
 	void monster1BtnPreDrawCallback(unsigned int shaderID, AQ_CompSimpleBox2D* simpleBox2DThis);
 
+	void startSelectionPageComps();
+
 	enum class AttackMode {
 		SELECTING,
 		ATTACKING
@@ -23,38 +25,7 @@ namespace attack {
 		selectionMonsterBtn0 = gameObjectCtrl->getComponent<CustomButtonComp>(gameObjectThis, "SelectionMonsterBtn0");
 		selectionMonsterBtn1 = gameObjectCtrl->getComponent<CustomButtonComp>(gameObjectThis, "SelectionMonsterBtn1");
 
-		int returnTexIndex{};
-		attackSelectionPage->addDiffuseTexture("assets/AttackSelectionPage.png", "AttackSelectionPageImageTex", GL_REPEAT, GL_REPEAT, GL_LINEAR, GL_LINEAR,
-			true, &returnTexIndex);
-		attackSelectionPage->activateTexture(GL_TEXTURE0);
-		attackSelectionPage->setShaderID(shaders.at(0).ID);
-		attackSelectionPage->setPreDrawCallback(selectionPagePreDrawCallback);
-
-		selectionMonsterBtn0->addDiffuseTexture("assets/SelectionMonster0.png", "Monster0DiffuseTex", GL_REPEAT, GL_REPEAT, GL_LINEAR, GL_LINEAR, true, &returnTexIndex);
-		glm::vec3 topLeft{ -0.5f,  0.5f, .0f };
-		glm::vec3 topRight{ 0.5f,  0.5f, .0f };
-		glm::vec3 bottomRight{ 0.5f, -0.5f, .0f };
-		glm::vec3 bottomLeft{ -0.5f, -0.5f, .0f };
-		selectionMonsterBtn0->setSensorRange(topLeft, topRight, bottomRight, bottomLeft);
-		selectionMonsterBtn0->activateTexture(GL_TEXTURE0);
-		selectionMonsterBtn0->setShaderID(shaders.at(0).ID);
-		selectionMonsterBtn0->keepAspectRatio();
-		selectionMonsterBtn0->setPreDrawCallback(monster0BtnPreDrawCallback);
-		selectionMonsterBtn0->transformTranslate(glm::vec3(-.1f, .4f, 0.f));
-		selectionMonsterBtn0->translateSensorRange(glm::vec3(-.1f, .4f, 0.f));
-		selectionMonsterBtn0->transformScale(glm::vec3(.6f, .6f, 1.f));
-		selectionMonsterBtn0->scaleSensorRange(glm::vec3(.6f, .6f, 1.f));
-
-		selectionMonsterBtn1->addDiffuseTexture("assets/SelectionMonster1.png", "Monster1DiffuseTex", GL_REPEAT, GL_REPEAT, GL_LINEAR, GL_LINEAR, true, &returnTexIndex);
-		selectionMonsterBtn1->setSensorRange(topLeft, topRight, bottomRight, bottomLeft);
-		selectionMonsterBtn1->activateTexture(GL_TEXTURE0);
-		selectionMonsterBtn1->setShaderID(shaders.at(0).ID);
-		selectionMonsterBtn1->keepAspectRatio();
-		selectionMonsterBtn1->setPreDrawCallback(monster1BtnPreDrawCallback);
-		selectionMonsterBtn1->transformTranslate(glm::vec3(.5f, -.4f, 0.f));
-		selectionMonsterBtn1->translateSensorRange(glm::vec3(.5f, -.4f, 0.f));
-		selectionMonsterBtn1->transformScale(glm::vec3(.6f, .6f, 1.f));
-		selectionMonsterBtn1->scaleSensorRange(glm::vec3(.6f, .6f, 1.f));
+		startSelectionPageComps();
 	}
 	void update(AQ_GameObjectCtrl* gameObjectCtrl, AQ_GameObject* gameObjectThis) {
 		if (currentScene == CurrentScene::ATTACK) {
@@ -106,6 +77,41 @@ namespace attack {
 				break;
 			}
 		}
+	}
+
+	void startSelectionPageComps() {
+		int returnTexIndex{};
+		attackSelectionPage->addDiffuseTexture("assets/AttackSelectionPage.png", "AttackSelectionPageImageTex", GL_REPEAT, GL_REPEAT, GL_LINEAR, GL_LINEAR,
+			true, &returnTexIndex);
+		attackSelectionPage->activateTexture(GL_TEXTURE0);
+		attackSelectionPage->setShaderID(shaders.at(0).ID);
+		attackSelectionPage->setPreDrawCallback(selectionPagePreDrawCallback);
+
+		selectionMonsterBtn0->addDiffuseTexture("assets/SelectionMonster0.png", "Monster0DiffuseTex", GL_REPEAT, GL_REPEAT, GL_LINEAR, GL_LINEAR, true, &returnTexIndex);
+		glm::vec3 topLeft{ -0.5f,  0.5f, .0f };
+		glm::vec3 topRight{ 0.5f,  0.5f, .0f };
+		glm::vec3 bottomRight{ 0.5f, -0.5f, .0f };
+		glm::vec3 bottomLeft{ -0.5f, -0.5f, .0f };
+		selectionMonsterBtn0->setSensorRange(topLeft, topRight, bottomRight, bottomLeft);
+		selectionMonsterBtn0->activateTexture(GL_TEXTURE0);
+		selectionMonsterBtn0->setShaderID(shaders.at(0).ID);
+		selectionMonsterBtn0->keepAspectRatio();
+		selectionMonsterBtn0->setPreDrawCallback(monster0BtnPreDrawCallback);
+		selectionMonsterBtn0->transformTranslate(glm::vec3(-.1f, .4f, 0.f));
+		selectionMonsterBtn0->translateSensorRange(glm::vec3(-.1f, .4f, 0.f));
+		selectionMonsterBtn0->transformScale(glm::vec3(.6f, .6f, 1.f));
+		selectionMonsterBtn0->scaleSensorRange(glm::vec3(.6f, .6f, 1.f));
+
+		selectionMonsterBtn1->addDiffuseTexture("assets/SelectionMonster1.png", "Monster1DiffuseTex", GL_REPEAT, GL_REPEAT, GL_LINEAR, GL_LINEAR, true, &returnTexIndex);
+		selectionMonsterBtn1->setSensorRange(topLeft, topRight, bottomRight, bottomLeft);
+		selectionMonsterBtn1->activateTexture(GL_TEXTURE0);
+		selectionMonsterBtn1->setShaderID(shaders.at(0).ID);
+		selectionMonsterBtn1->keepAspectRatio();
+		selectionMonsterBtn1->setPreDrawCallback(monster1BtnPreDrawCallback);
+		selectionMonsterBtn1->transformTranslate(glm::vec3(.5f, -.4f, 0.f));
+		selectionMonsterBtn1->translateSensorRange(glm::vec3(.5f, -.4f, 0.f));
+		selectionMonsterBtn1->transformScale(glm::vec3(.6f, .6f, 1.f));
+		selectionMonsterBtn1->scaleSensorRange(glm::vec3(.6f, .6f, 1.f));
 	}
 
 	void selectionPagePreDrawCallback(unsigned int shaderID, AQ_CompSimpleBox2D* simpleBox2DThis) {
