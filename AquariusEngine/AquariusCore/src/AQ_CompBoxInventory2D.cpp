a150c6e573849b9237a6afbf4ab2fb561554b8ed
TempleLo <templelo5408@gmail.com>
2021-12-01 05:38:38 +0800

    Implement definitions for AQ_CompBoxInventory2D's methods.

--- AquariusEngine/AquariusCore/src/AQ_CompBoxInventory2D.cpp
+++ AquariusEngine/AquariusCore/src/AQ_CompBoxInventory2D.cpp
@@ -1,1 +1,42 @@
 #include "headers/AQ_CompBoxInventory2D.hpp"
+
+namespace aquarius_engine {
+	void AQ_CompBoxInvertory2D::setSlotTexture(std::string imageLocation, unsigned int wrap_s, unsigned int wrap_t, unsigned int min_filter, unsigned int mag_filter, bool hasAndUseAlpha)
+	{
+	}
+	void AQ_CompBoxInvertory2D::setSlotsRange(glm::vec3 topLeftVertXYZ, glm::vec3 downRightVertXYZ)
+	{
+	}
+	void AQ_CompBoxInvertory2D::setSpacing(float spacing)
+	{
+	}
+	bool AQ_CompBoxInvertory2D::hoverCheck(double cursorX, double cursorY, bool normalized)
+	{
+		return false;
+	}
+	bool AQ_CompBoxInvertory2D::clickCheck(double cursorX, double cursorY, bool normalized)
+	{
+		return false;
+	}
+	void AQ_CompBoxInvertory2D::draw()
+	{
+	}
+	void AQ_CompBoxInvertory2D::disable(bool disable)
+	{
+	}
+	void AQ_CompBoxInvertory2D::transformReset()
+	{
+	}
+	void AQ_CompBoxInvertory2D::transformRotate(glm::vec3 rotateVector, float angle)
+	{
+	}
+	void AQ_CompBoxInvertory2D::transformTranslate(glm::vec3 translateVector)
+	{
+	}
+	void AQ_CompBoxInvertory2D::transformScale(glm::vec3 scaleVector)
+	{
+	}
+	void AQ_CompBoxInvertory2D::setTransform(glm::mat4 transform)
+	{
+	}
+}
