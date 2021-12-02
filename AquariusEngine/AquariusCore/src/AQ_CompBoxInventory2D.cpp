7054c4ba5682e8ab41002848d67a5b9b9e6e31bc
TempleLo <templelo5408@gmail.com>
2021-12-02 20:42:36 +0800

    Slight start of designing instancing shader for inventory's slots. Also fix unnoticed missing colon bug in AQSLTranspiler.

--- AquariusEngine/AquariusCore/src/AQ_CompBoxInventory2D.cpp
+++ AquariusEngine/AquariusCore/src/AQ_CompBoxInventory2D.cpp
@@ -1,11 +1,34 @@
 #include "headers/AQ_CompBoxInventory2D.hpp"
+#include "headers/stbi_image_wrapper.hpp"
+#include "headers/AQ_Shader.hpp"
 
 namespace aquarius_engine {
-	AQ_CompBoxInvertory2D::AQ_CompBoxInvertory2D(unsigned int shaderID, bool rowMajor) : AQ_CompSimpleBox2D(shaderID), AQ_IHoverClick(), rowMajor(rowMajor) {
+	unsigned int AQ_CompBoxInvertory2D::slotInstancingShaderID{ 0 };
 
+	AQ_CompBoxInvertory2D::AQ_CompBoxInvertory2D(unsigned int shaderID, bool rowMajor, unsigned int slotCount) 
+		: AQ_CompSimpleBox2D(shaderID), AQ_IHoverClick(), rowMajor(rowMajor), slotTexture(0), slotCount(slotCount) {
+		if (!slotInstancingShaderID) {
+
+		}
 	}
-	void AQ_CompBoxInvertory2D::setSlotTexture(std::string imageLocation, unsigned int wrap_s, unsigned int wrap_t, unsigned int min_filter, unsigned int mag_filter, bool hasAndUseAlpha)
-	{
+	void AQ_CompBoxInvertory2D::setSlotTexture(std::string imageLocation, unsigned int wrap_s, unsigned int wrap_t, unsigned int min_filter, unsigned int mag_filter, bool hasAndUseAlpha) {
+		if (slotTexture) {
+			glDeleteTextures(1, &slotTexture);
+		}
+		glGenTextures(1, &slotTexture);
+		glBindTexture(GL_TEXTURE_2D, slotTexture);
+		int width, height, nrChannels;
+		unsigned char* data = stbi_image_wrap::loadImage(imageLocation, width, height, nrChannels);
+		if (data) {
+			if (!hasAndUseAlpha)
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			else
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+			stbi_image_wrap::freeImage(data);
+		} else {
+			throw std::string("ERROR: CANNOT LOAD IMAGE WITH GIVEN PATH--" + imageLocation);
+		}
+		setTexWrapFilter(wrap_s, wrap_t, min_filter, mag_filter);
 	}
 	void AQ_CompBoxInvertory2D::setSlotsRange(glm::vec3 topLeftVertXYZ, glm::vec3 downRightVertXYZ)
 	{
